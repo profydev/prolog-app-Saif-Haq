99bdd6a372b92d8647e23d46db3cdadfefee2c41
Saif-Haq <itssaifhaq@gmail.com>
2024-04-02 14:16:19 +0500

    Added support mail instead of alert

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -83,7 +83,11 @@ export function SidebarNavigation() {
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              onClick={() =>
+                window.open(
+                  "mailto:support@prolog-app.com?subject=Support Request:",
+                )
+              }
             />
             <MenuItemButton
               text="Collapse"
